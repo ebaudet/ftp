28a60457a53aca7b0710d9d61a95428bd5fb87d0
Emilien BAUDET <ebaudet@e1r3p12.42.fr>
2014-05-14 22:21:33 +0200

    début

--- srcs/client.c
+++ srcs/client.c
@@ -6,13 +6,65 @@
 /*   By: ebaudet <ebaudet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/05/14 13:23:09 by ebaudet           #+#    #+#             */
-/*   Updated: 2014/05/14 13:25:03 by ebaudet          ###   ########.fr       */
+/*   Updated: 2014/05/14 20:33:30 by ebaudet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int main(int ac, char **av)
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include "libft.h"
+
+void	usage(char *str)
 {
-	(void)ac;
-	(void)av;
+	ft_putstr("Usage: ");
+	ft_putstr(str);
+	ft_putstr(" <addr> <port>\n");
+	exit(-1);
+}
+
+int		create_client(char *addr, int port)
+{
+	int						sock;
+	struct protoent			*proto;
+	struct sockaddr_in		sin;
+
+	proto = getprotobyname("tcp");
+	if (proto == 0)
+		return (-1);
+	sock = socket(PF_INET, SOCK_STREAM, proto->p_proto);
+	sin.sin_family = AF_INET;
+	sin.sin_port = htons(port);
+	sin.sin_addr.s_addr = inet_addr(addr);
+	if (connect(sock, (const struct sockaddr *)&sin, sizeof(sin)) == -1)
+	{
+		ft_putstr("Connect error");
+		exit(2);
+	}
+	return (sock);
+}
+
+int 	main(int ac, char **av)
+{
+	int						port;
+	int						sock;
+	/*int						r;
+	char					buf[1024];*/
+
+	if (ac != 3)
+		usage(av[0]);
+	port = ft_atoi(av[2]);
+	sock = create_client(av[1], port);
+	/*while ((r = read(cs, buf, 1023)) > 0)
+	{
+		buf[r] = '\0';
+		ft_putstr("message reçut : ");
+		ft_putendl(buf);
+	}*/
+	write(sock, "bonjour\n", 8);
+	close(sock);
 	return (0);
 }
--- srcs/serveur.c
+++ srcs/serveur.c
@@ -6,13 +6,70 @@
 /*   By: ebaudet <ebaudet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/05/14 13:23:03 by ebaudet           #+#    #+#             */
-/*   Updated: 2014/05/14 13:25:12 by ebaudet          ###   ########.fr       */
+/*   Updated: 2014/05/14 20:22:49 by ebaudet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int main(int ac, char **av)
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include "libft.h"
+
+void	usage(char *str)
 {
-	(void)ac;
-	(void)av;
+	ft_putstr("Usage: ");
+	ft_putstr(str);
+	ft_putstr(" <port>\n");
+	exit(-1);
+}
+
+int		create_server(int port)
+{
+	int						sock;
+	struct protoent			*proto;
+	struct sockaddr_in		sin;
+
+	proto = getprotobyname("tcp");
+	if (proto == 0)
+		return (-1);
+	sock = socket(PF_INET, SOCK_STREAM, proto->p_proto);
+	sin.sin_family = AF_INET;
+	sin.sin_port = htons(port);
+	sin.sin_addr.s_addr = htonl(INADDR_ANY);
+	if (bind(sock, (const struct sockaddr *)&sin, sizeof(sin)) == -1)
+	{
+		ft_putstr("Bind error");
+		exit(2);
+	}
+	listen(sock, 42);
+	return (sock);
+}
+
+int 	main(int ac, char **av)
+{
+	int						port;
+	int						sock;
+	int						cs;
+	unsigned int			cslen;
+	struct sockaddr_in		csin;
+	int						r;
+	char					buf[1024];
+
+	if (ac != 2)
+		usage(av[0]);
+	port = ft_atoi(av[1]);
+	sock = create_server(port);
+	cs = accept(sock, (struct sockaddr *)&csin, &cslen);
+	while ((r = read(cs, buf, 1023)) > 0)
+	{
+		buf[r] = '\0';
+		ft_putstr("message reçut : ");
+		ft_putendl(buf);
+	}
+	close(cs);
+	close(sock);
 	return (0);
 }
